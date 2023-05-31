4888e7ad02e8ab8f233d5cb93b76a4eaa693d6b3
dildeep15 <dildeep15@gmail.com>
2023-05-31 22:27:30 +0200

    Insert data into assistant table

--- SQL Scripts/05_insertSuperheroes.sql
+++ SQL Scripts/05_insertSuperheroes.sql
@@ -1,5 +1,5 @@
 USE SuperHeroesDB;
-INSERT INTO Superhero (Name, Alias, Origin)
-Values('Iron Man', 'IronMan', 'Origin of Iron Man'),
+INSERT INTO Assistant (Name, SuperheroId)
+Values('SpiderMan', 'IronMan', 'Origin of Iron Man'),
 ('Spider Man', 'SpiderMan', 'Origin of SpiderMan'),
 ('Batman', 'Batman', 'Origin of Batman');
---
+++ SQL Scripts/06_insertAssistants.sql.sql
@@ -0,0 +1,6 @@
+USE SuperHeroesDB;
+INSERT INTO Assistant (Name, SuperheroId)
+Values('Jarvis',1),
+('Ultron', 1),
+('Ned Leeds', 2),
+('Robin', 3);
