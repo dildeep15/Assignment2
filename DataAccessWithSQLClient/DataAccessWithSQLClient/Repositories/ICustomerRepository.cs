3c8074a2b017fc0f57122a6257f73b6cc119a7de
dildeep15 <dildeep15@gmail.com>
2023-06-03 12:47:39 +0200

    Added summary tags to ICustomerRepository Interface

--- DataAccessWithSQLClient/DataAccessWithSQLClient/Repositories/ICustomerRepository.cs
+++ DataAccessWithSQLClient/DataAccessWithSQLClient/Repositories/ICustomerRepository.cs
@@ -9,12 +9,35 @@ namespace DataAccessWithSQLClient.Repositories
 {
     internal interface ICustomerRepository
     {
-        // Declaration of function to be implemented for customer 
+        /// <summary>
+        /// <c>GetAllCustomer</c> method return all customers.
+        /// </summary>
+        /// <returns>List of customers</returns>
         public List<Customer> GetAllCustomer();
+        /// <summary>
+        /// <c>GetCustomerById</c> return a specific customer by customerId.
+        /// </summary>
+        /// <returns>Customer</returns>
         public Customer GetCustomerById();
+        /// <summary>
+        /// <c>GetCustomerByName</c> return a specific customer by name.
+        /// </summary>
+        /// <returns></returns>
         public Customer GetCustomerByName();
+        /// <summary>
+        /// <c>AddNewCustomer</c> add a new customer to database.
+        /// </summary>
+        /// <returns>Boolean true or false</returns>
         public bool AddNewCustomer();
+        /// <summary>
+        /// <c>UpdateCustomer</c> update a customer in database.
+        /// </summary>
+        /// <returns>Boolean true or false</returns>
         public bool UpdateCustomer();
+        /// <summary>
+        /// <c>GetCustomerByCountry</c> return customers group by country code.
+        /// </summary>
+        /// <returns>List of customers</returns>
         public List<Customer> GetCustomerByCountry();        
     }
 }
