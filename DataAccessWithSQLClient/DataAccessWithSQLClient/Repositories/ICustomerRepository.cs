c86dfbe9b5fa15e2be832cadfe5cb2cb6d030f9d
dildeep15 <dildeep15@gmail.com>
2023-06-03 10:22:40 +0200

    Created Genre Class & ICustomerRepository Interface

---
+++ DataAccessWithSQLClient/DataAccessWithSQLClient/Repositories/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessWithSQLClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessWithSQLClient.Repositories
+{
+    internal interface ICustomerRepository
+    {
+        // Declaration of function to be implemented for customer 
+        public List<Customer> GetAllCustomer();
+        public Customer GetCustomerById();
+        public Customer GetCustomerByName();
+        public bool AddNewCustomer();
+        public bool UpdateCustomer();
+        public List<Customer> GetCustomerByCountry();        
+    }
+}
